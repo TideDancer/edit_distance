e73554ae8376ef479fd8e7d623f90cb74a8c829b
Xingyu Cai <xic12006@XUS.engr.uconn.edu>
2016-03-06 00:43:47 -0500

    finish wihtout debug

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
-#define GAP -2
-#define MIS -3
-#define MATCH 2
+#define GAP 1
+#define MISMATCH 1
+#define MATCH 0
 
 int main(int argc, char* argv[]) {
 
-  char* s1, s2;
+  char* s1, s2, output_s, p;
   int** M;
   int len_s1,len_s2;
   int case1, case2, case3 = 0;
+  int i,j=0;
   
   if(argc != 3)
   	printf("Please Input 2 Sequences");
@@ -22,9 +23,9 @@ int main(int argc, char* argv[]) {
   len_s1 = strlen(s1);
   len_s2 = strlen(s2);	
 
-	M = (int**) malloc((len_s1j+1)*sizeof(int*))
-	for(int i=0; i<=len_s1; i++)
-	  M[i] = (int*) malloc((len_s2+1)*sizeof(int));
+  M = (int**) malloc((len_s1+1)*sizeof(int*))
+  for(int i=0; i<=len_s1; i++)
+    M[i] = (int*) malloc((len_s2+1)*sizeof(int));
 
   M[0][0] = 0;
   for(int i=1; i<=len_s1; i++)
@@ -42,9 +43,54 @@ int main(int argc, char* argv[]) {
 		else
 			case3 = M[i-1][j-1] + MISMATCH;
                 
-		case1 = max(case1, case2);
-		case1 = max(case1, case3);
+		case1 = min(case1, case2);
+		case1 = min(case1, case3);
 		M[i][j] = case1;
 	  }
+  
+  print("The edit distance is %d \n",M[len_s1][len_s2];
+
+  i = len_s1;
+  j = len_s2;
+  output_s = (char*) malloc((len_s1+len_s2+1)*sizeof(char));
+  p = output_s;
+
+  while(i>0 || j>0) 
+  {
+    case1 = min(M[i-1][j], M[i,j-1]);
+	case1 = min(case1, M[i-1][j-1]);
+	switch(case1)
+	{
+	  case M[i-1][j-1]:
+	    if(M[i-1][j-1] == M[i][j])
+		{ *p = '|'; p++; }
+		else
+		{ *p = '*'; p++; }
+		i--;
+		j--;
+		break;
+      case M[i-1][j]:
+        *p = "1";
+		i--;
+        break;
+      case M[i][j-1]:
+        *p = "2";
+		j--;
+        break;
+      default:
+        break;		
+	}
+  }
+  while(i>0)
+  { *p = "1"; i--; }
+  while(j>0)
+  { *p = "2"; j--; }
+
+  while(p != output_s)
+  {
+    printf("%s",*p);
+	p--;
+  }
+  pritnf("%s\n",*p);
 
 }
